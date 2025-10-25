1d29a6355e2b344daa218a54a47d1414df84ee63
jlbuild <juliabuildbot@gmail.com>
2025-10-25 17:02:33 +0000

    OpenJpeg_jll build 2.5.5+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -13,7 +13,7 @@ function __init__()
     JLLWrappers.@generate_init_header(LittleCMS_jll, libpng_jll, Libtiff_jll)
     JLLWrappers.@init_library_product(
         libopenjp2,
-        "lib/libopenjp2.2.5.3.dylib",
+        "lib/libopenjp2.2.5.4.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
